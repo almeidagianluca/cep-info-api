d81b628705701267a15eff0ce698efc6eed665e3
almeidagianluca <almeidagianluca@gmail.com>
2024-10-25 22:57:57 -0300

    initial commit

---
+++ src/test/java/io/github/almeidagianluca/cep_info_api/CepInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.almeidagianluca.cep_info_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
